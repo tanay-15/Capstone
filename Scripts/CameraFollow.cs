0d36138aa1a16c1a5a64db8abc3f00c1a5092367
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2018-09-01 22:48:05 -0400

    Enemy basic chasing

---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform target;
+    public float speed = 0.075f;
+    public Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, speed);
+        transform.position = smoothPosition;		
+	}
+}
---
+++ Scripts/Movement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float speed = 5;
+    private Rigidbody player;
+    private SpriteRenderer sprite;
+    private Animator myAnim;
+    bool facingRight;
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<Rigidbody>();
+        sprite = GetComponent<SpriteRenderer>();
+        myAnim = GetComponent<Animator>();
+        facingRight = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        float hAxis = Input.GetAxis("Horizontal");
+        myAnim.SetFloat("Speed", Mathf.Abs(hAxis));
+        Vector3 movement = new Vector3(hAxis, 0, 0) * speed * Time.deltaTime;
+        player.MovePosition(transform.position + movement);
+
+        if(hAxis > 0 && !facingRight)
+        {
+            flip();
+        }
+        else if (hAxis<0 && facingRight)
+        {
+            flip();
+        }
+
+        if (Input.GetKey(KeyCode.Tab))
+        {
+           // player.enabled = false;
+            sprite.enabled = false;
+
+            if(!facingRight)
+            {
+                Vector3 targetPos = transform.position + transform.right * -0.25f;
+                player.transform.position = targetPos;
+                Invoke("Enabler", 0.5f);
+            }       
+            else
+            {
+                Vector3 targetPos = transform.position + transform.right * 0.25f;
+                player.transform.position = targetPos;
+                Invoke("Enabler", 0.5f);
+            }
+        }
+	}
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+    void Enabler()
+    {
+        //player.enabled = true;
+        sprite.enabled = true;
+    }
+}
