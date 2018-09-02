6d6a032aea7d57f35948ab98dbb20c8c7d6859da
Tanay Joshi <tanay.joshi1990@gmail.com>
2018-09-02 12:38:23 -0400

    damage

--- Prototype/Assets/Scripts/attackTrigger.cs
+++ Prototype/Assets/Scripts/attackTrigger.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class attackTrigger : MonoBehaviour {
+    public float damage = 10;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void OnTriggerEnter(Collider col)
+    {
+        if(col.isTrigger != true && col.CompareTag("Enemy"))
+        {
+            col.SendMessageUpwards("applyDamage", damage);
+        }
+    }
 }
