4994c31ea25937a1b5f92ab0dcf9aa30875171d2
Tanay Joshi <tanay.joshi1990@gmail.com>
2018-09-02 16:04:34 -0400

    code changes

--- Prototype/Assets/Scripts/playerAttack.cs
+++ Prototype/Assets/Scripts/playerAttack.cs
@@ -10,13 +10,16 @@ public class playerAttack : MonoBehaviour {
     public Collider attackTrigger;
     private Vector3 currentAngle;
     private Vector3 reset;
+    private Vector3 oldPos;
 	// Use this for initialization
 	void Start () {
         myAnim = GetComponent<Animator>();
         reset = attackTrigger.transform.localEulerAngles;
+        oldPos = attackTrigger.transform.position;
         attackTrigger.enabled = false;
-        
-	}
+      
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -25,16 +28,21 @@ public class playerAttack : MonoBehaviour {
             currentAngle.z = reset.z;
             attacking = true;
             attackTrigger.enabled = true;
-            attackTrigger.transform.localEulerAngles = new Vector3(attackTrigger.transform.localEulerAngles.x, attackTrigger.transform.localEulerAngles.y, currentAngle.z);
+            //attackTrigger.transform.localEulerAngles = new Vector3(attackTrigger.transform.localEulerAngles.x, attackTrigger.transform.localEulerAngles.y, currentAngle.z);
+            //attackTrigger.transform.localEulerAngles = new Vector3(0, 90, 0);
+            attackTrigger.transform.rotation = Quaternion.FromToRotation(Vector3.up, Vector3.right);
+            //attackTrigger.transform.localPosition = Vector3.Lerp(attackTrigger.transform.localPosition, new Vector3(1, 0, 0), 0.01f);
             //attackTrigger.transform.RotateAround(Vector3.zero, new Vector3(0,0,1), 45 * Time.deltaTime);
             //attackTrigger.transform.localPosition = Vector3.Slerp(attackTrigger.transform.localPosition, new Vector3(1, 0, 0), 0.01f);
             //attackTrigger.transform.localPosition = Vector3.Slerp(attackTrigger.transform.localPosition, new Vector3(1, 0, 0), 0.01f);
         }
-        else
+        else if(Input.GetMouseButtonUp(0))
         {
+            attackTrigger.transform.rotation = Quaternion.FromToRotation(Vector3.right, Vector3.up);
+            attackTrigger.transform.position = oldPos;
             attackTrigger.enabled = false;
+            
         }
 		
-        
 	}
 }
