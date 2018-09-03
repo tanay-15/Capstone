0ae0d7484d8eddfb0ff4f2eb0f37edce787aa7f2
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2018-09-03 19:35:39 -0400

    Range enemy

---
+++ Prototype/Assets/Scripts/RangeEnemy.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeEnemy : Enemy {
+
+
+    [Header("Range details")]
+    public GameObject shurikenprefab;
+    public GameObject shuriloct;
+    private GameObject go;
+
+
+	// Use this for initialization
+	void Start () {
+
+        anim = this.GetComponent<Animator>();
+        rigi = this.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (IsAlive)
+        {
+            DetectingPlayer();
+            Patrol();
+            Pursuit();
+            Attack();
+        }
+
+        Death();
+    }
+
+    public override void Attack()
+    {
+
+        if (AttackReady)
+        {
+            rateofattack -= Time.deltaTime;
+
+            if(rateofattack <= 0f)
+            {
+                //Throw shruriken
+                currentstate = States.Attack;
+                anim.SetBool("ShouldPursuit", false);
+                anim.SetTrigger("Attack");
+                if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+                {
+                    go = Instantiate(shurikenprefab, shuriloct.transform.position, shurikenprefab.transform.rotation);
+                    go.transform.SetParent(this.gameObject.transform);
+                    rateofattack = 3f;
+                }
+              
+            }
+        }
+
+        /*
+         *   if (AttackReady)
+        {
+            rateofattack -= Time.deltaTime;
+            if(rateofattack <= 0)
+            {
+              
+               
+                
+                Debug.Log("Enemy now attacks the player");
+                rateofattack = 2f;
+            }
+           
+        }
+         */
+        
+    }
+
+    public override void Pursuit()
+    {
+
+        if (target && !AttackReady)
+        {
+            currentstate = States.Pursuit;
+            anim.SetBool("ShouldPursuit", true);
+            this.transform.position = Vector2.MoveTowards(this.transform.position, targetpos, movspeed * Time.deltaTime);
+
+
+            if(Vector2.Distance(this.transform.position,target.transform.position) <= 3f)
+            {
+                targetpos = target.transform.position;
+                AttackReady = true;
+            }
+        }
+
+    }
+
+    public Vector2 GetTargetpos()
+    {
+        return targetpos;
+    }
+
+    public GameObject GetTarget()
+    {
+        return target;
+    }
+}
---
+++ Prototype/Assets/Scripts/RangedShuri.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedShuri : MonoBehaviour {
+
+    // Use this for initialization
+    public Vector2 targetposition;
+    public GameObject goparent;
+
+    private Vector2 movepos;
+
+   
+	void Start () {
+
+        targetposition = this.transform.GetComponentInParent<RangeEnemy>().GetTarget().transform.position;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(targetposition.x < this.transform.position.x)
+        {
+            movepos = new Vector2(-3f * Time.deltaTime, 0);
+        }
+       
+        else if(targetposition.x > this.transform.position.x)
+        {
+            movepos = new Vector2(3f * Time.deltaTime, 0);
+        }
+      this.transform.Translate(movepos);
+       
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
