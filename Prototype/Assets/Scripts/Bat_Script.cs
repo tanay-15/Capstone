c7203b1abe5b5d1146c5f1a84f9ff62581dfe796
RahulSVasudevan <rahulsusheel@gmail.com>
2018-09-03 12:35:52 -0400

    demon mode update

---
+++ Prototype/Assets/Scripts/Bat_Script.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat_Script : MonoBehaviour {
+
+    float theta = 0;
+    float radius = 8;
+
+    Vector3 offset;
+
+    bool begin = false;
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+        offset = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0);
+
+        int randomInt = (int)Random.Range(0f, 4f);
+
+        if (randomInt == 0)
+            theta = 0;
+        else if(randomInt == 1)
+            theta = 30;
+        else if (randomInt == 2)
+            theta = 60;
+        else if (randomInt == 3)
+            theta = 90;
+
+        Debug.Log(randomInt);
+
+        StartCoroutine("DelayBegin");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (begin)
+        {
+            theta += 2 * Time.deltaTime;
+            radius -= 3 * Time.deltaTime;
+
+            if (radius < 0)
+            {
+                GetComponent<Animator>().SetBool("Explode", true);
+                transform.localScale = new Vector3(2, 2, 1);
+            }
+            else
+            {
+                transform.position = player.transform.position + new Vector3(radius * Mathf.Sin(theta), radius * Mathf.Cos(theta)) + offset;
+            }
+        }
+    }
+
+    /*
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Destroy(gameObject);
+    }
+    */
+
+    IEnumerator DelayBegin()
+    {
+        yield return new WaitForSeconds(Random.Range(0f,1f));
+
+        begin = true;
+
+        yield return new WaitForSeconds(5);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Prototype/Assets/Scripts/DemonModeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonModeScript : MonoBehaviour {
+
+    public GameObject bat;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("z"))
+        {
+            StartCoroutine("Transform");
+            for (int i = 0; i < 25; i++)
+            {
+                var Bat = Instantiate(bat, transform.position + new Vector3(0,-10,0), Quaternion.identity);
+                Bat.GetComponent<Bat_Script>().player = gameObject;
+            }
+        }
+
+	}
+
+    IEnumerator Transform()
+    {
+        yield return new WaitForSeconds(3);
+        transform.GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(1).gameObject.SetActive(true);
+    }
+}
