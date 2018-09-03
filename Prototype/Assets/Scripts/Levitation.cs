728e835f52882952a65e3705d1a018b9a4243293
Joshua Rand <jrand35@aol.com>
2018-09-03 18:42:23 -0400

    Levitation complete

--- Prototype/Assets/Scripts/Levitation.cs
+++ Prototype/Assets/Scripts/Levitation.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Levitation : MonoBehaviour {
 
     public static Levitation sharedInstance;
-    public GameObject particles;
+    public Color nonHoverColor;
+    public Color hoverColor;
+    public ParticleSystem particles;
     [System.NonSerialized]
     public GameObject heldObject;
+
+    float grabRadius = 0.5f;
+    Vector3 mousePosition;
+    IEnumerable<Collider> collidingObjects;
 
     static Levitation()
     {
@@ -27,9 +34,73 @@ public class Levitation : MonoBehaviour {
 
         heldObject = null;
 	}
+
+    void CalculatePosition()
+    {
+        mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.gameObject.transform.position.z);
+        particles.gameObject.transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+
+    void UpdateColor()
+    {
+        collidingObjects = from col in Physics.OverlapSphere(Camera.main.ScreenToWorldPoint(mousePosition), grabRadius).ToList()
+                               where col.gameObject.tag == "Grabbable"
+                               select col;
+
+        ParticleSystem.MainModule main = particles.main;
+        if (collidingObjects.Count() > 0)
+        {
+            main.startColor = hoverColor;
+        }
+        else
+        {
+            main.startColor = nonHoverColor;
+        }
+    }
+
+    void PickUpObject(GameObject obj)
+    {
+        heldObject = obj;
+        heldObject.GetComponent<Rigidbody>().useGravity = false;
+        heldObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+
+    void ReleaseObject()
+    {
+        heldObject.GetComponent<Rigidbody>().useGravity = true;
+        heldObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        heldObject = null;
+    }
+
+    void CheckForButtonPress()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (heldObject == null)
+            {
+                Collider hoveringObject = collidingObjects.SingleOrDefault();
+                if (hoveringObject != null)
+                    PickUpObject(hoveringObject.gameObject);
+            }
+            else
+            {
+                ReleaseObject();
+            }
+        }
+    }
+
+    void MoveHeldObject()
+    {
+        if (heldObject != null)
+        {
+            heldObject.transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+        }
+    }
 	
 	void Update () {
-        Vector3 mouseOnNearClipPlane = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.gameObject.transform.position.z);
-        particles.transform.position = Camera.main.ScreenToWorldPoint(mouseOnNearClipPlane);
+        CalculatePosition();
+        UpdateColor();
+        CheckForButtonPress();
+        MoveHeldObject();
 	}
 }
