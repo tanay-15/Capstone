6d0e6ecc10b8900c4263bd7bbce609fb7b3eab4d
Joshua Rand <jrand35@aol.com>
2018-09-04 17:09:47 -0400

    Levitation icon and camera waypoints

--- Prototype/Assets/Scripts/CameraFollow.cs
+++ Prototype/Assets/Scripts/CameraFollow.cs
@@ -7,15 +7,35 @@ public class CameraFollow : MonoBehaviour {
     public float speed = 0.075f;
     public Vector3 offset;
 
+    public Transform lowerLeftBound;
+    public Transform upperRightBound;
+
 
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+    Vector3 Vector3Clamp(Vector3 input, Vector3 min, Vector3 max)
+    {
+        float x = Mathf.Clamp(input.x, min.x, max.x);
+        float y = Mathf.Clamp(input.y, min.y, max.y);
+        float z = Mathf.Clamp(input.z, min.z, max.z);
+
+        return new Vector3(x, y, z);
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        Vector3 desiredPosition = target.position + offset;
+        Vector3 desiredPosition = Vector3.zero;
+        if (lowerLeftBound == null || upperRightBound == null)
+        {
+            desiredPosition = target.position + offset;
+        }
+        else
+        {
+            desiredPosition = Vector3Clamp(target.position, lowerLeftBound.position, upperRightBound.position) + offset;
+        }
         Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, speed);
         transform.position = smoothPosition;		
 	}
