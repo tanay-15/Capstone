7e6c0c3063bc8907a5ae3fe8235fa23aab0b6531
Pratik Prasad Shringarpure <prati@DESKTOP-HI2QGSK>
2018-09-07 09:16:24 -0400

    Boss1

---
+++ Prototype/Assets/Scripts/Boss Scripts/Boss1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss1 : MonoBehaviour {
+
+    // Use this for initialization
+
+    [Header("Player Details")]
+    public GameObject player;
+
+    public enum State
+    {
+        Idle,
+        Walk,
+        ReadyToAttack,
+        GetHit,
+        Dead,
+        Attack1,
+        Attack2,
+        Reanimate
+    }
+    [Header("Boss Details")]
+    public State currentState;
+    public bool hasBarrier = true;
+    public int barrCounter = 3;
+
+	void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void applyDamage(int damage)
+    {
+        Debug.Log("Boss has been attacked!");
+    }
+}
--- Prototype/Assets/Scripts/ShadowSlash.cs
+++ Prototype/Assets/Scripts/ShadowSlash.cs
@@ -6,7 +6,9 @@ public class ShadowSlash : MonoBehaviour {
 
     // Use this for initialization
     private Animator anim;
-    private bool slashHit;
+    public bool slashHit;
+    public bool inslash;
+    public GameObject target;
 	void Start () {
 
         anim = GetComponent<Animator>();
@@ -16,8 +18,9 @@ public class ShadowSlash : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.M))
         {
+            Debug.Log("Use ShadowSlash");
             anim.SetTrigger("ShadowSlash");
         }
 
@@ -28,6 +31,14 @@ public class ShadowSlash : MonoBehaviour {
         if ((collider.gameObject.tag == "Enemy") && slashHit)
         {
             Debug.Log("Shadow Slash Connects on " + collider.name);
+            collider.SendMessageUpwards("applyDamage", 5);
+        }
+
+        if(collider.gameObject.tag == "Enemy")
+        {
+            Debug.Log(collider.gameObject.name);
+            inslash = true;
+            target = collider.gameObject;
         }
     }
 
@@ -36,11 +47,19 @@ public class ShadowSlash : MonoBehaviour {
         if(collider.gameObject.tag == "Enemy")
         {
             slashHit = false;
+            inslash = false;
+            target = null;
+           
         }
     }
 
     void SlashHit()
     {
         slashHit = true;
+        if (inslash)
+        {
+            Debug.Log("Shadow Slash connects on " + target.name);
+            target.SendMessageUpwards("applyDamage", 10);
+        }
     }
 }
