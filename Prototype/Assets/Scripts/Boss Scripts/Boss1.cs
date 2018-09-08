48c89c286872a5864061fa7d6487184890390f90
Pratik Prasad Shringarpure <prati@DESKTOP-HI2QGSK>
2018-09-08 10:46:46 -0400

    Attack 2 Dark Orbs

--- Prototype/Assets/Scripts/Boss Scripts/Boss1.cs
+++ Prototype/Assets/Scripts/Boss Scripts/Boss1.cs
@@ -24,19 +24,93 @@ public class Boss1 : MonoBehaviour {
     public State currentState;
     public bool hasBarrier = true;
     public int barrCounter = 3;
+    public float barrierback = 10;
+
+    [Header("Attack Shield Down")]
+    public GameObject orbprefab;
+    public GameObject orbloc1;
+    public GameObject orbloc2;
+    public GameObject orbloc3;
+    public GameObject orbloc4;
+    public bool shouldattack2;
+    public float attack2counter = 0;
+
+    private Quaternion o3rot;
+    
 
 	void Start () {
 
         player = GameObject.FindGameObjectWithTag("Player");
+       // o3rot = Quaternion.Euler(0, 0, -23.5);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        BarrierLogic();
+
+        if (shouldattack2)
+        {
+            attack2counter = attack2counter - Time.deltaTime;
+            if(attack2counter <= 0)
+            {
+                ShieldDownAttack();
+                attack2counter = 10;
+            }
+          
+           
+        }
 	}
+
+    public void ShieldDownAttack()
+    {
+        //attack when shield is down
+
+        Instantiate(orbprefab, orbloc1.transform.position, orbloc1.transform.rotation);
+        Instantiate(orbprefab, orbloc2.transform.position, orbloc2.transform.rotation);
+        Instantiate(orbprefab, orbloc3.transform.position,orbloc3.transform.rotation);
+        Instantiate(orbprefab, orbloc4.transform.position, orbloc4.transform.rotation);
+        
+    }
+
+
+
+    public void BarrierLogic()
+    {
+
+        //logic for barrier
+        if (!hasBarrier)
+        {
+            shouldattack2 = true;
+            barrierback = barrierback - Time.deltaTime;
+          
+
+            if (barrierback <= 0)
+            {
+                barrCounter = 2;
+                hasBarrier = true;
+                barrierback = 10;
+                Debug.Log("Barrier is back up!");
+                shouldattack2 = false;
+            }
+        }
+    }
 
     public void applyDamage(int damage)
     {
         Debug.Log("Boss has been attacked!");
+        if (barrCounter >= 0)
+        {
+           
+            Debug.Log("Barrier strength is : " + barrCounter);
+            barrCounter--;
+        }
+
+        if(barrCounter < 0)
+        {
+            Debug.Log("Barrier down!");
+            hasBarrier = false;
+        }
+       
     }
 }
---
+++ Prototype/Assets/Scripts/Boss Scripts/DarkOrbs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkOrbs : MonoBehaviour {
+
+    // Use this for initialization
+
+    private Vector3 moveVector;
+	void Start () {
+
+        moveVector = new Vector3(-5 * Time.deltaTime, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.Translate(moveVector);
+	}
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hits " + collision.gameObject.name);
+        Destroy(this.gameObject);
+    }
+}
