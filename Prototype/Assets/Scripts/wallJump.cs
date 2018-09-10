7b583fae0ca06d6b8e92cfdc65058fbc837cabfb
Tanay Joshi <tanay.joshi1990@gmail.com>
2018-09-10 01:14:44 -0400

    walljump

---
+++ Prototype/Assets/Scripts/wallJump.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallJump : MonoBehaviour
+{
+    private Movement playerMovement;
+    private Grounded ground;
+    // Use this for initialization
+    void Start()
+    {
+        playerMovement = GetComponent<Movement>();
+        ground = GetComponent<Grounded>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && ground.grounded == false)
+        {
+
+        }
+
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
