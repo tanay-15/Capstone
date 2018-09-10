94ab6fc0275c3ca5448b8d17729c3e4069018943
Tanay Joshi <tanay.joshi1990@gmail.com>
2018-09-10 18:33:23 -0400

    weapon pickups incomplete

---
+++ Prototype/Assets/Scripts/weaponManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponManager : MonoBehaviour {
+
+    public GameObject activeWeapon;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void updateWeapon(GameObject newWeapon)
+    {
+        activeWeapon = newWeapon;
+        GetComponent<SpriteRenderer>().sprite = activeWeapon.GetComponent<SpriteRenderer>().sprite;
+    }
+}
---
+++ Prototype/Assets/Scripts/weaponPickup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponPickup : MonoBehaviour {
+
+    public GameObject[] weapons;
+    public GameObject weaponHere;
+
+    // Use this for initialization
+    void Start()
+    {
+        weaponHere = weapons[Random.Range(0, weapons.Length)];
+        GetComponent<SpriteRenderer>().sprite = weaponHere.GetComponent<SpriteRenderer>().sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "player")
+        {
+            other.transform.Find("handEnd").GetComponent<weaponManager>().updateWeapon(weaponHere);
+        }
+    }
+}
