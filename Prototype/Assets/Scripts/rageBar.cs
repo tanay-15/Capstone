c598c4313db159c8aa9a078e14d98d02c45447a1
Tanay Joshi <tanay.joshi1990@gmail.com>
2018-09-12 13:04:21 -0400

    rageBar initial conditions done

--- Prototype/Assets/Scripts/rageBar.cs
+++ Prototype/Assets/Scripts/rageBar.cs
@@ -7,26 +7,54 @@ public class rageBar : MonoBehaviour {
 
     [SerializeField]
     private float fillAmount;
-
-
+    private bool rageBarActive;
+    private rageBar RBar;
+    private float newValue;
+    private float updatedValue;
+    public float speedtoDecrease;
+    public float speedtoIncrease;
     [SerializeField]
     private Image filler;
 
    
                                        // Use this for initialization
     void Start () {
-        
+        rageBarActive = false;
+        RBar = GetComponent<rageBar>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         handleBar();
+
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            if (rageBarActive)
+            {
+                rageBarActive = false;
+            }
+            else
+            {
+                rageBarActive = true;
+            }      
+        }
+
+        if(rageBarActive)
+        {
+            Invoke("rageBarDeplete", 3f);
+        }
+        else if (!rageBarActive)
+        {
+            Invoke("rageBarRegen", 5f);
+        }
+
+        
 	}
     private void handleBar()
     {
-        if(fillAmount != filler.fillAmount)
+        if(RBar.fillAmount != filler.fillAmount)
         {
-            filler.fillAmount = fillAmount;
+            filler.fillAmount = RBar.fillAmount;
         }
         
     }
@@ -34,5 +62,18 @@ public class rageBar : MonoBehaviour {
     private float Mapping(float value, float inMin, float inMax,float outMin,float outMax)
     {
         return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+
+    private void rageBarDeplete()
+    {
+        newValue = Mapping(RBar.fillAmount, 0,100,0,1);
+        updatedValue = Mathf.Lerp(RBar.fillAmount, newValue, Time.deltaTime * speedtoDecrease);
+        RBar.fillAmount = updatedValue;
+    }
+
+    private void  rageBarRegen()
+    {
+        newValue = Mapping(100, 0, 100, 0, 1);
+        RBar.fillAmount = Mathf.Lerp(RBar.fillAmount, newValue, Time.deltaTime * speedtoIncrease);  
     }
 }
