b1d328fc7b0cf45f8e90b38e8710943ff14f5b4a
RahulSVasudevan <rahulsusheel@gmail.com>
2018-09-13 23:48:55 -0400

    ragdoll start

---
+++ Prototype/Assets/Scripts/2Dscripts/RagdollEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollEnemy : MonoBehaviour {
+
+
+    Component[] bones;
+
+    public bool isDead = false;
+
+    void Start ()
+    {
+
+        bones = gameObject.transform.GetComponentsInChildren<Rigidbody2D>();
+
+        SetChildrenKinematic(true);
+    }
+	
+
+	void Update () {
+
+        if (isDead == true)
+        {
+            SetChildrenKinematic(false);
+        }
+
+	}
+
+    void SetChildrenKinematic(bool kinematic)
+    {
+        foreach (Rigidbody2D rb2d in bones)
+        {
+            if(rb2d.name != "Stickman")
+                rb2d.isKinematic = kinematic;
+        }
+    }
+}
