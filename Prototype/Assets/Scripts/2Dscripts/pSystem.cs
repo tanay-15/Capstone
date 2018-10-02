72a53b8357bf69d97329b5407554750f1231862c
Tanay Joshi <tanay.joshi1990@gmail.com>
2018-10-01 23:06:32 -0400

    Basic particle system for walking

---
+++ Prototype/Assets/Scripts/2Dscripts/pSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pSystem : MonoBehaviour
+{
+    public ParticleSystem particleLeft;
+    public ParticleSystem particleRight;
+    // Use this for initialization
+    void Start()
+    {
+        particleLeft = GetComponent<ParticleSystem>();
+        particleRight = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision col)
+    {
+        if (col.collider.tag != null)
+        {
+            particleLeft.Play();
+            particleRight.Play();
+        }
+    }
+    void OnCollisionExit2D(Collision col)
+    {
+        if (col.collider.tag != null)
+        {
+            particleLeft.Stop();
+            particleRight.Stop();
+        }
+    }
+}
