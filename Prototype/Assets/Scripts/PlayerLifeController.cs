dfaf2deec79ec02d8f1f55067dcb37182406885f
Joshua Rand <jrand35@aol.com>
2018-10-04 23:49:32 -0400

    Implemented screen shake for taking hits and auto-enemy deletion

--- Prototype/Assets/Scripts/PlayerLifeController.cs
+++ Prototype/Assets/Scripts/PlayerLifeController.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class PlayerLifeController : MonoBehaviour {
 
+    SpriteRenderer[] renderers;
+
 	void Start () {
-		
+        renderers = GetComponentsInChildren<SpriteRenderer>();
 	}
 	
 	void Update () {
@@ -29,13 +31,30 @@ public class PlayerLifeController : MonoBehaviour {
         if (col.gameObject.tag == "Enemy")
         {
             PlayerLife.sharedInstance.AddLife(-10);
+            FindObjectOfType<CameraFollow>().ShakeCamera();
+            BlinkRed(true);
+            Invoke("Unblink", 0.1f);
             
+        }
+    }
+
+    //Eventually make these two into one function or coroutine
+    void Unblink()
+    {
+        BlinkRed(false);
+    }
+
+    void BlinkRed(bool blink)
+    {
+        foreach (SpriteRenderer r in renderers)
+        {
+            r.color = (blink) ? Color.red : Color.white;
         }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-
+        
         if (col.gameObject.tag == "Instant Death")
         {
             ResetLevel();
