0378861db782e759cab78b846564e355fb5462ca
Tanay Joshi <tanay.joshi1990@gmail.com>
2018-10-16 23:48:49 -0400

    rolling,wip

---
+++ Prototype/Assets/Scripts/2Dscripts/Rolling.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rolling : MonoBehaviour {
+    Rigidbody2D player;
+    private Movement2D playerMovement;
+    // Use this for initialization
+    void Start () {
+        player = GetComponent<Rigidbody2D>();
+        playerMovement = GetComponent<Movement2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            // player.enabled = false;
+            // sprite.enabled = false;
+            //sprite.enabled = false;
+
+            if (!playerMovement.facingRight)
+            {
+                Vector3 targetPos = transform.position + transform.right * -0.25f;
+                player.transform.position = targetPos;
+                //Add rolling animation left
+            }
+            else
+            {
+                Vector3 targetPos = transform.position + transform.right * 0.25f;
+                player.transform.position = targetPos;
+                //Add rolling animation right
+            }
+        }
+
+    }
+}
