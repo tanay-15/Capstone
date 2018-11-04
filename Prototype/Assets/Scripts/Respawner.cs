36daba461f2932906ba04abbe6c3c4a8a2bbc42e
Joshua Rand <jrand35@aol.com>
2018-11-03 20:45:14 -0400

    The player can no longer lose throwing objects in Tutorial2

---
+++ Prototype/Assets/Scripts/Respawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawner : MonoBehaviour {
+
+    public GameObject[] trackedObjects;
+    Vector3[] originalPosition;
+
+    void Start()
+    {
+        originalPosition = new Vector3[trackedObjects.Length];
+        for (int i = 0; i < trackedObjects.Length; i++)
+        {
+            originalPosition[i] = trackedObjects[i].transform.position;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        for (int i = 0; i < trackedObjects.Length; i++)
+        {
+            if (trackedObjects[i] == col.gameObject)
+            {
+                col.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                col.gameObject.GetComponent<Rigidbody2D>().angularVelocity = 0f;
+                col.gameObject.transform.position = originalPosition[i];
+            }
+        }
+    }
+}
