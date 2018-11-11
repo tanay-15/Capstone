e908cc1bb239902b1557461f2eb49c34fe0c84c1
Pratik Prasad Shringarpure <prati@DESKTOP-HI2QGSK>
2018-11-11 01:04:04 -0500

    Basic World Switch

---
+++ Prototype/Assets/Scripts/WorldSwitch.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldSwitch : MonoBehaviour {
+
+    // Use this for initialization
+
+    public bool DemonWorld = false;
+
+    public GameObject player;
+    public GameObject playermode_Hero;
+    public GameObject playermode_Demon;
+
+    public Sprite sprite1;
+    public Sprite sprite2;
+
+    public GameObject[] walls;
+
+	void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        walls = GameObject.FindGameObjectsWithTag("Wall");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (playermode_Demon.activeSelf)
+        {
+            DemonWorld = true;
+        }
+        else if(playermode_Hero.activeSelf)
+        {
+            DemonWorld = false;
+        }
+
+        Switching();
+
+	}
+
+    void Switching()
+    {
+        if (DemonWorld)
+        {
+            foreach(GameObject go in walls)
+            {
+                go.GetComponent<SpriteRenderer>().sprite = sprite2;
+            }
+        }
+
+        if (!DemonWorld)
+        {
+            foreach(GameObject go in walls)
+            {
+                go.GetComponent<SpriteRenderer>().sprite = sprite1;
+            }
+        }
+    }
+}
