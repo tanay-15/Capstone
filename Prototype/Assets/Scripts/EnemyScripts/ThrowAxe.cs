45ffa09d07f2c715ffbc5f7dbe33864c1d0860ea
Pratik Prasad Shringarpure <prati@DESKTOP-HI2QGSK>
2019-01-21 01:16:48 -0500

    AxeSkeleton update

--- Prototype/Assets/Scripts/EnemyScripts/ThrowAxe.cs
+++ Prototype/Assets/Scripts/EnemyScripts/ThrowAxe.cs
@@ -7,23 +7,57 @@ public class ThrowAxe : MonoBehaviour {
     // Use this for initialization
     //Move in -x
 
-    private Vector3 moveVector;
+    private Vector2 moveVector;
     private Rigidbody2D rigi;
+
+    private GameObject player_target;
+    private Vector3 startpoint;
+    private float TimeToHit = 2f;
     
 	void Start () {
 
         //moveVector = new Vector3(-8f * Time.deltaTime, 0, 0);
         rigi = this.GetComponent<Rigidbody2D>();
-        moveVector = 8*Vector3.Normalize(GameObject.Find("Character").transform.position- transform.position);
+        player_target = GameObject.Find("Character");
+        startpoint = this.transform.position;
+         moveVector = 4*Vector3.Normalize(GameObject.Find("Character").transform.position- transform.position);
+        //Physics2D.gravity = new Vector2(0, -9.8f);
+     
 
+    }
+
+    void AxeThrow()
+    {
+       
+        Vector3 distance = startpoint - player_target.transform.position;
+        distance.z = 0f;
+        Vector2 distanceCalc = distance;
+        distanceCalc.y = 0f;
+
+        float Sy = distance.y;
+        float Sx = distanceCalc.magnitude;
+
+        float Vx = Sx / TimeToHit;
+        float Vy = Sy / TimeToHit + 0.5f * Mathf.Abs(Physics.gravity.y) * TimeToHit;
+
+        Vector2 result = distanceCalc.normalized;
+
+        result = result * Vx;
+        result.y = Vy;
+
+        moveVector = result;
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
 
         //this.transform.Translate(moveVector);
+
+        //transform.rotation = Quaternion.LookRotation(moveVector);
         rigi.velocity = moveVector;
-	}
+
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
