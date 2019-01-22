b62baf9d223d266068973c0e4ad4da1941f57129
Joshua Rand <JRand@student.dwc.edu>
2019-01-22 12:10:25 -0500

    Created gates for skeleton midboss

---
+++ Prototype/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class UnityEventCollider2D : UnityEvent<Collider2D>
+{
+
+}
+
+public class TriggerEvent : MonoBehaviour
+{
+    public UnityEventCollider2D TriggerEnter;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        TriggerEnter.Invoke(collision);
+    }
+}
