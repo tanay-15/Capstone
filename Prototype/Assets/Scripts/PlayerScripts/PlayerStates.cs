126b7fac91c16ad641b38c6571142bc565a4dbce
Joshua Rand <jrand35@aol.com>
2019-02-16 19:54:15 -0500

    Re-added camera shake line

--- Prototype/Assets/Scripts/PlayerScripts/PlayerStates.cs
+++ Prototype/Assets/Scripts/PlayerScripts/PlayerStates.cs
@@ -222,6 +222,7 @@ public class PlayerStates : MonoBehaviour
                     if (grounded == true)
                     {
                         Destroy(Instantiate(DustParticles, GroundTrigger.transform.position, Quaternion.identity), 2f);
+                        FindObjectOfType<CameraFollow>().ShakeCamera();
                         DustParticles.Play();
                         
                         status = State.Default;
