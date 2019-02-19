114385a93aec151397d13ad057269995ea94088a
lutem <sxl4023@g.rit.edu>
2019-02-18 23:43:18 -0500

    files for orb escort

---
+++ Prototype/Assets/Scripts/OrbPuzzle_End.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbPuzzle_End : MonoBehaviour
+{
+    // game object you want to affect
+    public GameObject target;
+
+    // only do once
+    bool triggered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggered = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("Orb") && !triggered)
+        {
+            // do whatever with target object
+
+            // destroy orb
+            Destroy(collision.gameObject);
+            triggered = true;
+        }
+    }
+}
---
+++ Prototype/Assets/Scripts/OrbPuzzle_Start.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// lutem's orb puzzle component, starting position
+
+public class OrbPuzzle_Start : MonoBehaviour
+{
+
+    public GameObject orbPrefab;
+    public float timeAlive;
+
+    bool orbPresent;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Instantiate(orbPrefab, this.transform);
+        orbPresent = true;
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!orbPresent)
+        {
+            timer += Time.deltaTime;
+            if (timeAlive < timer)
+            {
+                if (this.transform.childCount > 0)
+                {
+                    Destroy(this.transform.GetChild(0).gameObject);
+                }
+                Instantiate(orbPrefab, this.transform);
+                orbPresent = true;
+                timer = 0f;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("Orb"))
+        {
+            orbPresent = false;
+        }
+    }
+}
