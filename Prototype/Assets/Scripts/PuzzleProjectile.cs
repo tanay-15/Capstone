c620daa6f8c8397597eaf23a9cffd909c3acdef3
lutem <sxl4023@g.rit.edu>
2019-02-20 17:42:55 -0500

    shooter prefab added

---
+++ Prototype/Assets/Scripts/PuzzleProjectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleProjectile : MonoBehaviour
+{
+    // speed at travel
+    public float speed;
+
+    Vector3 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set target 10 units in direction
+        target = this.transform.position + (transform.up * 10.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, target, speed * Time.deltaTime);
+        if(this.transform.position == target)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+
+        if(collision.gameObject.tag == "ground")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Prototype/Assets/Scripts/PuzzleShooter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleShooter : MonoBehaviour
+{
+    public GameObject projectile;
+
+    // speed at which projectile fires
+    public float speed;
+
+    // rate of fire
+    public float fireRate;
+
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0.0f;
+
+        projectile.GetComponent<PuzzleProjectile>().speed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > fireRate)
+        {
+            timer = 0.0f;
+            Instantiate(projectile,this.transform);
+            
+        }
+    }
+}
