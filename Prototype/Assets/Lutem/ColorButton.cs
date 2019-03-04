6a827368ef417793af994a0ee0a3ecad42b3a9d7
Joshua Rand <jrand35@aol.com>
2019-03-03 23:39:58 -0500

    Added button graphic to tutorial

--- Prototype/Assets/Lutem/ColorButton.cs
+++ Prototype/Assets/Lutem/ColorButton.cs
@@ -6,6 +6,11 @@ using UnityEngine.Events;
 public class ColorButton : MonoBehaviour {
     public UnityEvent OnPressed;
     public bool pressed;
+    public bool overrideColor = false;
+    public Color color;
+
+    [System.NonSerialized]
+    public Color myColor;
 
     List<GameObject> colliders;
 
@@ -14,6 +19,8 @@ public class ColorButton : MonoBehaviour {
     {
         colliders = new List<GameObject>();
         pressed = false;
+
+        myColor = (overrideColor) ? color : GetComponent<SpriteRenderer>().color;
     }
 
     // Update is called once per frame
@@ -32,7 +39,7 @@ public class ColorButton : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.GetComponent<SpriteRenderer>().color == this.GetComponent<SpriteRenderer>().color)
+        if (collision.gameObject.GetComponent<SpriteRenderer>().color == myColor)
         {
             OnPressed.Invoke();
             colliders.Add(collision.gameObject);
@@ -41,7 +48,7 @@ public class ColorButton : MonoBehaviour {
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.GetComponent<SpriteRenderer>().color == this.GetComponent<SpriteRenderer>().color)
+        if (collision.gameObject.GetComponent<SpriteRenderer>().color == myColor)
         {
             colliders.Remove(collision.gameObject);
         }
