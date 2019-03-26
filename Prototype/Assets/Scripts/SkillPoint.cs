2acafb978362c176260cb9747bc5a40dbbfcdadf
Joshua Rand (RIT Student) <jar8659@ad.rit.edu>
2019-03-26 12:49:44 -0400

    Added skill point prefab

---
+++ Prototype/Assets/Scripts/SkillPoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillPoint : MonoBehaviour
+{
+    public SpriteRenderer white;
+    public Light myLight;
+    float count;
+    float amplitude = 0.15f;
+    float haloBaseScale = 1.3f;
+    float haloAmplitude = 0.4f;
+    float countScale = 3f;
+    Color newColor;
+    void Start()
+    {
+        newColor = Color.white;
+    }
+
+    void Update()
+    {
+        count += Time.deltaTime;
+        newColor.a = amplitude + amplitude * -Mathf.Cos(count * countScale);
+        myLight.range = haloBaseScale + (haloAmplitude * 0.5f) + (haloAmplitude * 0.5f) * -Mathf.Cos(count * countScale);  //1.3 to 1.7
+        white.color = newColor;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
