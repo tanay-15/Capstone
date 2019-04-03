155aa7cf54c70622f6d15ef00776acc73ef87bbd
kunalpatill <kunalpatil95@gmail.com>
2019-04-03 14:01:17 -0400

    parallax

---
+++ Prototype/Assets/Scripts/ParallaxBG.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBG : MonoBehaviour
+{
+    private float length, startPosX, startPosY;
+    public GameObject Cam;
+    public float parallaxAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosX = transform.position.x;
+        startPosY = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = (Cam.transform.position.x * (1 - parallaxAmount));
+        float distX = (Cam.transform.position.x * parallaxAmount);
+        float distY = (Cam.transform.position.y * parallaxAmount);
+
+        transform.position = new Vector3(startPosX + distX, transform.position.y, transform.position.z);
+        if (temp > startPosX + length) startPosX += length;
+        else if (temp < startPosX - length) startPosX -= length;
+    }
+}
