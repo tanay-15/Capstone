9d12052e48ff456b7e86084af0618d9250cd4de5
lutem <sxl4023@g.rit.edu>
2019-04-04 12:03:07 -0400

    updates to falling platform script and lvl 2

--- Prototype/Assets/Lutem/FallingPlatform.cs
+++ Prototype/Assets/Lutem/FallingPlatform.cs
@@ -42,7 +42,7 @@ public class FallingPlatform : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player" && collision.gameObject.transform.position.y > this.transform.position.y)
+        if(collision.gameObject.tag == "Player" && (collision.gameObject.transform.position.y + 0.3f) > this.transform.position.y)
         {
             if (timed && !triggered)
             {
