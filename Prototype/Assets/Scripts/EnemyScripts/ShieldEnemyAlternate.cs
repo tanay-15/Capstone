f467356dfe0282c59e59b71ab6521c5f9a566c43
Tanay Joshi <tanay.joshi1990@gmail.com>
2019-04-03 23:58:44 -0400

    shield enemy wip

--- Prototype/Assets/Scripts/EnemyScripts/ShieldEnemyAlternate.cs
+++ Prototype/Assets/Scripts/EnemyScripts/ShieldEnemyAlternate.cs
@@ -10,26 +10,41 @@ public class ShieldEnemyAlternate : Enemy
     // Start is called before the first frame update
     void Start()
     {
+        LookingLeft = true;
         currentstate = States.Idle;
         anim = GetComponent<Animator>();
     }
     IEnumerator Charge()
     {
         //play running animation
+        Debug.Log("before playing animation");
         anim.Play("AltShield_AttackReady");
         var dir = target.transform.position - this.transform.position;
         var dis = dir.magnitude;
         finalDirection = dir / dis;
-        yield return new WaitForSeconds(2f);       
+        yield return new WaitForSeconds(2f);
+        //transform.GetComponent<Rigidbody2D>().velocity = new Vector2(finalDirection.x, 0).normalized * speed;
+        //transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(finalDirection.x, 0).normalized * speed, ForceMode2D.Impulse);
+        Debug.Log("velocity : " + transform.GetComponent<Rigidbody2D>().velocity);
         anim.Play("AltShield_Attack");
+
         //transform.position = Vector2.MoveTowards(transform.position, target.transform.position, 0.25f);
         //transform.GetComponent<Rigidbody2D>().velocity =  new Vector2(finalDirection.x, 0).normalized * speed;
         //transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(finalDirection.x, 0).normalized * speed, ForceMode2D.Impulse);
-        Debug.Log("velocity : " + transform.GetComponent<Rigidbody2D>().velocity);
+       
     }
     // Update is called once per frame
     void Update()
     {
+        if(finalDirection.x > 0 && LookingLeft)
+        {
+            flip();
+
+        }
+        else if(finalDirection.x < 0 && !LookingLeft)
+        {
+            flip();
+        }
         switch (currentstate)
         {
             case States.Idle:
@@ -39,47 +54,31 @@ public class ShieldEnemyAlternate : Enemy
                     if (elapsedTime > 3.0f && target && withinRange)
                     {
                         elapsedTime = 0f;
-
                         StartCoroutine("Charge");
+
                         currentstate = States.Attack;
                     }
                     break;
                 }
             case States.Attack:
                 {
-
-
-
                     transform.GetComponent<Rigidbody2D>().velocity = new Vector2(finalDirection.x, 0).normalized * speed;
-                    //if ((target.transform.position - transform.position).magnitude < 0.5f)
-                    //{
-
-                    //    currentstate = States.KnockBack;
-                    //}
-
-
+                    Debug.Log("velocity " + transform.GetComponent<Rigidbody2D>().velocity);
                     if (!withinRange)
                     {
                         currentstate = States.Idle;
                     }
-                    
-
-
                     break;
                 }
             case States.KnockBack:
                 {
-                    
-                    transform.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
-                    Debug.Log("velocity " + transform.GetComponent<Rigidbody2D>().velocity);
-                    target.GetComponent<Rigidbody2D>().AddForce(finalDirection * 2f, ForceMode2D.Impulse);
-                    
-                    currentstate = States.Idle;
-
-
-                   
-
-                    break;
+                 
+                  transform.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+                  Debug.Log("velocity " + transform.GetComponent<Rigidbody2D>().velocity);
+                  target.GetComponent<Rigidbody2D>().AddForce(finalDirection * 2f, ForceMode2D.Impulse);
+                  
+                  currentstate = States.Idle;
+                  break;
                 }
                 
         }
