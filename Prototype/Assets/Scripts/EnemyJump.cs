9ce56827f6f05149d06e4a16608e831cd4adddcd
Pratik Prasad Shringarpure <prati@DESKTOP-HI2QGSK>
2019-04-04 11:19:05 -0400

    Jump Enemy New Prefab

--- Prototype/Assets/Scripts/EnemyJump.cs
+++ Prototype/Assets/Scripts/EnemyJump.cs
@@ -13,28 +13,70 @@ public class EnemyJump : MonoBehaviour
 
     public GameObject AttackPoint;
     private Vector3 FinalJumpPosition;
+
+    public int JumpCount = 0;
+    public bool JumpComplete = false;
+
+    public bool LookingLeft = true;
     void Start()
     {
-        
+        JumpComplete = false;
+        FinalJumpPosition = AttackPoint.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         AttackBehaviour();
+     
     }
 
 
     void AttackBehaviour()
     {
-        //Just kick the ball
 
-        if (Input.GetKeyDown(KeyCode.Space))
+
+
+        if (!JumpComplete)
         {
-            FinalJumpPosition = AttackPoint.transform.position;
-            Attack(FinalJumpPosition);
+            JumpAttack();
+            JumpComplete = true;
         }
 
+
+        if(Vector3.Distance(this.transform.position,FinalJumpPosition) < 2f)
+        {
+            Debug.Log("Made my jump!");
+            JumpCount = JumpCount + 1;
+            if (JumpCount >= 3)
+            {
+                flip();
+                JumpCount = 0;
+            }
+            FinalJumpPosition = AttackPoint.transform.position;
+            Invoke("JumpAgain", 2f);
+           
+        }
+
+       
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Attack(FinalJumpPosition);
+        //}
+
+    }
+
+    void JumpAgain()
+    {
+   
+        JumpComplete = false;
+    }
+
+    void JumpAttack()
+    {
+       
+        Attack(FinalJumpPosition);
     }
 
     Vector3 CalculateLaunchVelocity(Vector3 _FinalJumpPosition)
@@ -46,10 +88,7 @@ public class EnemyJump : MonoBehaviour
         Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * h * gravity);
         Vector3 velocityXZ = displacementXZ / (Mathf.Sqrt(-2 * h / gravity) + 0.885f /*1.7785f Mathf.Sqrt(2*(displacementY-h))/gravity*/);
 
-        Debug.Log(Mathf.Sqrt(-2 * h / gravity));
-        Debug.Log(Mathf.Sqrt(2 * (displacementY - h)) / gravity);
-        Debug.Log(displacementY);
-        Debug.Log(velocityXZ);
+
 
         return velocityXZ + velocityY;
     }
@@ -58,5 +97,22 @@ public class EnemyJump : MonoBehaviour
     {
         Physics.gravity = Vector3.up * gravity;
         this.GetComponent<Rigidbody2D>().velocity = CalculateLaunchVelocity(_JumpPosition);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void flip()
+    {
+
+        LookingLeft = !LookingLeft;
+
+        this.transform.localScale = new Vector3(this.transform.localScale.x * -1, this.transform.localScale.y, this.transform.localScale.z);
+
     }
 }
