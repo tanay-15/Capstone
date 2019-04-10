1dd0043fceb5cbe9db1d2fe263ee8bd443940a4f
kunalpatill <kunalpatil95@gmail.com>
2019-04-10 14:09:50 -0400

    parallax error fixes

---
+++ Prototype/Assets/Scripts/ParallaxGameElements.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxGameElements : MonoBehaviour
+{
+    private float startPosX, startPosY;
+    public GameObject Cam;
+    public float parallaxAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosX = transform.position.x;
+        startPosY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (Cam.transform.position.x * (1 - parallaxAmount));
+        float distX = (Cam.transform.position.x * parallaxAmount);
+        float distY = (Cam.transform.position.y * parallaxAmount);
+
+        transform.position = new Vector3(startPosX + distX, transform.position.y, transform.position.z);
+    }
+}
