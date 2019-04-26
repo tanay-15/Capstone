5676d12b356bd36b19c74b8711ff2090fc7ea745
Joshua Rand (RIT Student) <jar8659@ad.rit.edu>
2019-04-25 21:17:19 -0400

    Added tracker script

---
+++ Prototype/Assets/Scripts/Tracker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tracker : MonoBehaviour
+{
+    public GameObject obj;
+    public Text text;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = obj.transform.position.ToString();
+    }
+}
