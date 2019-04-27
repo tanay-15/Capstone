0fb0311d9f99938c3f11d9a53d27474a3cbdb2da
Joshua Rand <jrand35@aol.com>
2019-04-27 13:11:06 -0400

    Update PauseMenu.cs

--- Prototype/Assets/Scripts/PauseMenu.cs
+++ Prototype/Assets/Scripts/PauseMenu.cs
@@ -75,12 +75,16 @@ public class PauseMenu : MonoBehaviour {
     }
 	
 	void Update () {
-        if ((Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P) || Input.GetButtonDown("PS4Options") || Input.GetButtonDown("PS4CIRCLE")) && (menuState == PauseMenuState.Main || menuState == PauseMenuState.Options) && !PauseMenuDisabled)
+        if ((Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P) || Input.GetButtonDown("PS4Options")) && (menuState == PauseMenuState.Main || menuState == PauseMenuState.Options) && !PauseMenuDisabled)
         {
             PauseUnpause();
         }
         if (GamePaused)
         {
+            if (Input.GetButtonDown("PS4CIRCLE"))
+            {
+                PauseUnpause();
+            }
             CheckAxis();
             if (menuState == PauseMenuState.Main || menuState == PauseMenuState.Options)
             {
